693f24fecea6949021956a8360221f9474e2a519
AlejandroC1991 <alejandro.celiberto@gmail.com>
2023-03-22 17:01:53 -0300

    cambios

--- desafioEntrega1302/app.js
+++ desafioEntrega1302/app.js
@@ -86,40 +86,40 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.get('/session', (req, res) => {
-    if (req.session.counter) {
-        req.session.counter++;
-        res.send(`Se ha vistio el sitio ${req.session.counter} veces`);
-    } else {
-        req.session.counter = 1;
-        res.send(`Bienvenido`);
-    }
-});
+// app.get('/session', (req, res) => {
+//     if (req.session.counter) {
+//         req.session.counter++;
+//         res.send(`Se ha vistio el sitio ${req.session.counter} veces`);
+//     } else {
+//         req.session.counter = 1;
+//         res.send(`Bienvenido`);
+//     }
+// });
 
 
 
-app.get('/login', (req, res) => {
-    const { username, password } = req.query;
+// app.get('/login', (req, res) => {
+//     const { username, password } = req.query;
 
-    if (username !== 'pepe' || password !== 'pepepass') {
-        return res.send('login fallido');
-    }
+//     if (username !== 'pepe' || password !== 'pepepass') {
+//         return res.send('login fallido');
+//     }
 
-    req.session.user = username;
-    req.session.admin = true;
-    res.send('Login exitoso');
-});
+//     req.session.user = username;
+//     req.session.admin = true;
+//     res.send('Login exitoso');
+// });
 
-app.get('/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (!err) res.send('Logout exitoso')
-        else res.send({ error: 'error', body: err });
-    });
-});
+// app.get('/logout', (req, res) => {
+//     req.session.destroy(err => {
+//         if (!err) res.send('Logout exitoso')
+//         else res.send({ error: 'error', body: err });
+//     });
+// });
 
-app.get('/privado', auth, (req, res) => {
-    res.send('Estas logueado');
-});
+// app.get('/privado', auth, (req, res) => {
+//     res.send('Estas logueado');
+// });
 
 app.listen(8080, () => console.log('Server running'));
 

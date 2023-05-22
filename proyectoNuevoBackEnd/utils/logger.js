5dfafbd67e71aa3c1705552b78d03ed801dc6b9f
AlejandroC1991 <alejandro.celiberto@gmail.com>
2023-05-22 19:04:54 -0300

    loggers

--- proyectoNuevoBackEnd/utils/logger.js
+++ proyectoNuevoBackEnd/utils/logger.js
@@ -1,27 +1,90 @@
 import winston from "winston";
 import * as dotenv from 'dotenv';
 
+
 dotenv.config();
 
 const ENVIRONMENT = process.env.NODE_ENV;
+const DEVELOPMENT = process.env.NODE_DEV;
 
-// const customLevelOptions = {
-//     levels: {
-//         fatal: 0,
-//         error: 1,
-//         warning: 2,
-//         info: 3,
-//         debug: 4
-//     },
-//     colors: {
-//         'fatal': 'red',
-//         'error': 'red',
-//         'warning': 'yellow',
-//         'info': 'green',
-//         'debug': 'blue'
-//     }
-// }
+const desarrollo = {
+    levels: {
+        fatal: 0,
+        error: 1,
+        warning: 2,
+        info: 3,
+        http: 4,
+        debug: 5
+    },
+    colors: {
+        'fatal': 'red',
+        'error': 'red',
+        'warning': 'yellow',
+        'info': 'green',
+        'http': 'green',
+        'debug': 'blue'
+    }
+}
 
+// Custom logger
+const logger = winston.createLogger({
+    levels: desarrollo.levels,
+    transports: [
+        new winston.transports.Console({
+            level: 'debug',
+            format: winston.format.combine(
+                winston.format.colorize({
+                    all: true,
+                    colors: desarrollo.colors
+                }),
+                winston.format.simple()
+            )
+        }),
+        // new winston.transports.File({
+        //     filename: 'logs/dev_custom.log',
+        //     level: 'fatal'
+        // })
+    ]
+});
+
+const produccion = {
+    levels: {
+        fatal: 0,
+        error: 1,
+        warning: 2,
+        info: 3,
+        http: 4,
+        debug: 5
+    },
+    colors: {
+        'fatal': 'red',
+        'error': 'red',
+        'warning': 'yellow',
+        'info': 'green',
+        'http': 'green',
+        'debug': 'blue'
+    }
+}
+// Custom logger
+const loggerProd = winston.createLogger({
+    levels: produccion.levels,
+    transports: [
+        new winston.transports.Console({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.colorize({
+                    all: true,
+                    colors: produccion.colors
+                }),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename: 'logs/errors.log',
+            level: 'error'
+        })
+    ]
+});
 // const logger = winston.createLogger({
 //     transports: [
 //         new winston.transports.Console({
@@ -35,54 +98,42 @@ const ENVIRONMENT = process.env.NODE_ENV;
 // });
 
 //Ejercicio multientorno
-let logger;
+// let logger;
 
-if (ENVIRONMENT === 'production') {
-    logger = winston.createLogger({
-        transports: [
-            new winston.transports.Console({
-                level: 'http'
-            }),
-            new winston.transports.File({
-                filename: 'logs/prod.log',
-                level: 'warn'
-            })
-        ]
-    })
-} else {
-    logger = winston.createLogger({
-        transports: [
-            new winston.transports.Console({
-                level: 'verbose'
-            })
-        ]
-    })
-}
+// if (ENVIRONMENT === 'production') {
+//     logger = winston.createLogger({
+//         transports: [
+//             new winston.transports.Console({
+//                 level: 'http'
+//             }),
+//             new winston.transports.File({
+//                 filename: 'logs/prod.log',
+//                 level: 'warn'
+//             })
+//         ]
+//     })
+// } else {
+//     logger = winston.createLogger({
+//         transports: [
+//             new winston.transports.Console({
+//                 level: 'verbose'
+//             })
+//         ]
+//     })
+// }
 
-//Custom logger
-// const logger = winston.createLogger({
-//     levels: customLevelOptions.levels,
-//     transports: [
-//         new winston.transports.Console({
-//             level: 'debug',
-//             format: winston.format.combine(
-//                 winston.format.colorize({
-//                     all: true,
-//                     colors: customLevelOptions.colors
-//                 }),
-//                 winston.format.simple()
-//             )
-//         }),
-//         new winston.transports.File({
-//             filename: 'logs/dev_custom.log',
-//             level: 'fatal'
-//         })
-//     ]
-// });
+
 
 export const addLogger = (req, res, next) => {
     req.logger = logger;
-    req.logger.http(`${req.method} en ${req.url} - ${new Date().toISOString()}`);
-    // req.logger.info(`${req.method} en ${req.url} - ${new Date().toISOString()}`);
+    // req.logger.http(`${req.method} en ${req.url} - ${new Date().toISOString()}`);
+    req.logger.debug(`${req.method} en ${req.url} - ${new Date().toISOString()}`);
+    next();
+}
+
+export const addLoggerProd = (req, res, next) => {
+    req.loggerProd = loggerProd;
+    // req.logger.http(`${req.method} en ${req.url} - ${new Date().toISOString()}`);
+    req.logger.info(`${req.method} en ${req.url} - ${new Date().toISOString()}`);
     next();
 }
